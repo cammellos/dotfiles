4aa42cb8426e211dc4590bf51b07924e3a0e6827
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2015-09-20 23:28:08 +0100

    git abbreviations

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,3 +1,11 @@
-
 . "$HOME/.homesick/repos/homeshick/homeshick.fish"
+
+abbr -a gst="git status"
+abbr -a gco="git checkout"
+abbr -a gl="git pull --rebase"
+abbr -a gp="git push"
+abbr -a gcp="git cherry-pick"
+abbr -a gre="git rebase"
+abbr -a ga="git add"
+abbr -a gc="git commit"
 
