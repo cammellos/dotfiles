f7b12419cf4d3513b36beb02baa06bd016dbe20d
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2015-09-29 14:03:24 +0100

    screenlayout

---
+++ home/.screenlayout/MML269.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-1 --off --output DisplayPort-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 3840x0 --rotate normal --output eDP-0 --mode 1920x1080 --pos 1920x808 --rotate normal
